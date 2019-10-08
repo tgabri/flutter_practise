567162eee419b5b9c3ef7e61c3b8e2ef56f8339f
Gabor <tgabri@gmail.com>
2019-10-08 11:05:17 +0100

    first app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import './product_manager.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.cyan, accentColor: Colors.indigo),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager('Food Tester'),
+      ),
+    );
+  }
+}
